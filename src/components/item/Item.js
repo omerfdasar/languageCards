875297e77d55afa1eefe76df2e623dd3f5c86f68
Omer <omerfarukdasar@gmail.com>
2022-03-26 16:33:12 +0100

    some effect added

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import "./Item.css";
 const Item = ({ card }) => {
+  const [showLogo, setShowLogo] = useState(true);
   return (
-    <div className="card">
-      <img className="card-logo" src={card.img} />
-      <h3 className="card-title">{card.name}</h3>
+    <div className="card" onClick={() => setShowLogo(!showLogo)}>
+      {showLogo ? (
+        <div>
+          <img className="card-logo" src={card.img} />
+          <h3 className="card-title">{card.name}</h3>
+        </div>
+      ) : (
+        <ul className="list">
+          {card.options.map((option, index) => {
+            return (
+              <li key={index}>
+                {option}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
